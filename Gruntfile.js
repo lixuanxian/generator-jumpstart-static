6cd8a2fd3f4104e17c39d15d066dbc759574c845
maxbarry <maxbarrymail@gmail.com>
2014-10-22 21:38:48 +0100

    Removing some dead weight from the GruntFile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,32 +2,10 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        copyto: {
-            release: {
-                files: [
-                    {
-                        cwd: "",
-                        src: ["**/*"],
-                        dest: "<%= pkg.paths.release %>",
-                        expand: true,
-                        dot: true
-                    }
-                ],
-                options: {
-                    ignore: [
-                        "node_modules/**/*",
-                        "Gruntfile.js",
-                        "*.sublime-project",
-                        "*.sublime-workspace"
-                    ]
-                }
-            }
-        },
         bump: {
             options: {
                 push: false,
-                commit: false,
-                createTag: false,
+                pushTo: "origin",
                 files: [
                     "package.json"
                 ]
@@ -35,11 +13,6 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-copy-to');
     grunt.loadNpmTasks('grunt-bump');
-
-    grunt.registerTask('patch', ['bump:patch', 'copyto:release',]);
-    grunt.registerTask('minor', ['bump:minor', 'copyto:release',]);
-    grunt.registerTask('major', ['bump:major', 'copyto:release',]);
 
 };
