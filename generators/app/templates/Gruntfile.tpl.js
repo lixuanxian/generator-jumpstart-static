4c2e2507f15da5515f8d5262350f53bc178511b4
maxbarry <maxbarrymail@gmail.com>
2015-12-01 17:52:36 +0000

    Readded base directory for connect

--- generators/app/templates/Gruntfile.tpl.js
+++ generators/app/templates/Gruntfile.tpl.js
@@ -114,6 +114,7 @@ module.exports = function(grunt) {
             server: {
                 options: {
                     keepalive: true,
+                    base: "<%%= pkg.paths.build %>"
                 }
             }
         },
